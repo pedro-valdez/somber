e1e313a550154baeec06be031385f91f9b72dde0
pedro valdez <pedro@valdez>
2022-05-24 18:43:30 -0500

    Added getStaticPaths

--- pages/[gender]/[wear].js
+++ pages/[gender]/[wear].js
@@ -1,4 +1,5 @@
 import ProductList from "../../components/ProductList"
+import sanityClient from "../../lib/client"
 
 export default function Wear() {
   return (
@@ -9,4 +10,37 @@ export default function Wear() {
       <ProductList />
     </>
   )
+}
+
+export async function getStaticPaths() {
+  const wear = await sanityClient.fetch(`
+    * [_type == "category" 
+    && ( "menu-item" in parents[]->slug.current
+    || "menu-item" in parents[]->parents[]->slug.current  )] {
+      "wear": slug.current,
+    }
+  `)
+
+  const genders = await sanityClient.fetch(`
+    * [_type ==  "category" && "gender" in parents[]->slug.current] {
+      "gender": slug.current,
+    }
+  `)
+
+  const paths = genders
+    .map(gender => (wear
+      .map(wear => ({ params: { ...gender, ...wear  } }))))
+    .flat()
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+    },
+  }
 }
