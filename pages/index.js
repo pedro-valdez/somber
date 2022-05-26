cba5932ed035b22d6d2b254e74d0241f73ebfbb6
pedro valdez <pedro@valdez>
2022-05-26 16:30:18 -0500

    Now displays popular and newest products

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,52 @@
 import Hero from "../components/Hero"
 import CategoryPreview from "../components/CategoryPreview"
 import ProductList from "../components/ProductList"
+import sanityClient from "../lib/client"
 
-export default function Home() {
+export default function Home({ popular, newest }) {
   return (
     <>
       <Hero />
       <CategoryPreview />
-      <ProductList />
-      <ProductList />
+      <ProductList products={popular} />
+      <ProductList products={newest} />
     </>
   )
+}
+
+export async function getStaticProps() {
+  const popular = await sanityClient.fetch(`
+    * [_type == "product"
+    && "popular" in tags] {
+      "id": _id,
+      "name": title,
+      "href": "#",
+      "price": defaultProductVariant.price,
+      "imageSrc": defaultProductVariant.images[0].asset->url,
+      "imageAlt": title,
+    }
+  `)
+
+  const newest = await sanityClient.fetch(`
+    * [_type == "product"] {
+      "id": _id,
+      "name": title,
+      "href": "#",
+      "price": defaultProductVariant.price,
+      "imageSrc": defaultProductVariant.images[0].asset->url,
+      "imageAlt": title,
+      _createdAt,
+    }
+    | order(_createdAt desc)
+    [0..3]
+  `)
+
+  console.log(newest)
+
+  return {
+    props: {
+      popular,
+      newest,
+    },
+  }
 }
