b36a9c608c46013b1fae6d600016733390aa9a3b
pedro valdez <pedro@valdez>
2022-05-28 00:29:06 -0500

    Added a cart
    As of a right now, it's a dumb component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar"
+import Cart from "../components/Cart"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <Cart />
       <Component {...pageProps} />
     </>
   )
