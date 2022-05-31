3e8ea31cf1f0832de8605801333bbc4346bb2864
pedro valdez <pedro@valdez>
2022-05-30 23:55:50 -0500

    Context provided for cart and navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import Navbar from "../components/Navbar"
+import CartProvider from "../components/CartContext"
 import Cart from "../components/Cart"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
-      <Cart />
+      <CartProvider>
+        <Navbar />
+        <Cart />
+      </CartProvider>
       <Component {...pageProps} />
     </>
   )
