b559d80253fd4f8c687e7006aa0868b3a0e8e689
pedro valdez <pedro@valdez>
2022-05-31 13:46:27 -0500

    Product of different size is added as different item in cart
    Code was also formatted

--- components/CartContext.js
+++ components/CartContext.js
@@ -10,10 +10,26 @@ export const CartContext = createContext()
 function cartReducer(state, action) {
   switch(action.type) {
     case CART_ACTIONS.ADD:
-      return [...state, action.payload]
+      const newProduct = {
+        ...action.payload,
+        id: action.payload.id + action.payload.selectedSize
+      }
+
+      const newProductIndex = state
+        .findIndex(product => product.id === newProduct.id)
+
+      if(newProductIndex > -1) {
+        const newState = JSON.parse(JSON.stringify(state))
+        newState[newProductIndex].quantity += newProduct.quantity
+        return newState
+      }
+
+      return [...state, newProduct]
       break
+
     case CART_ACTIONS.REMOVE:
       return state.filter(product => product.id !== action.payload)
+
     default:
       return state
   }
