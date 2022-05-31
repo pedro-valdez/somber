9f4f913adb379c8bf84d43b6bcb4c5440728400a
pedro valdez <pedro@valdez>
2022-05-31 00:04:35 -0500

    Navbar can now open cart

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import { Fragment, useState } from 'react'
+import { useCartContext } from "./CartContext"
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import { MenuIcon, SearchIcon, ShoppingBagIcon, XIcon } from '@heroicons/react/outline'
 
@@ -131,6 +132,7 @@ function classNames(...classes) {
 
 export default function Navbar() {
   const [open, setOpen] = useState(false)
+  const { setOpen: setCartOpen } = useCartContext()
 
   return (
     <div className="bg-white sticky top-0 left-0 w-full z-50">
@@ -436,14 +438,18 @@ export default function Navbar() {
 
                 {/* Cart */}
                 <div className="ml-4 flow-root lg:ml-6">
-                  <a href="#" className="group -m-2 p-2 flex items-center">
+                  <button
+                    href="#"
+                    className="group -m-2 p-2 flex items-center text-left"
+                    onClick={() => setCartOpen(true)}
+                  >
                     <ShoppingBagIcon
                       className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
                       aria-hidden="true"
                     />
                     <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                     <span className="sr-only">items in cart, view bag</span>
-                  </a>
+                  </button>
                 </div>
               </div>
             </div>
