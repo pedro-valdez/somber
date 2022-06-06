0e72554e9a2621a1f33d288373d524f4c991c5ec
pedro valdez <pedro@valdez>
2022-06-06 14:54:07 -0500

    Added checkout page

---
+++ pages/checkout.js
@@ -0,0 +1,9 @@
+import CheckoutForm from "../components/CheckoutForm"
+
+export default function Checkout() {
+  return (
+    <div className="max-w-5xl mx-auto my-5">
+      <CheckoutForm />
+    </div>
+  )
+}
